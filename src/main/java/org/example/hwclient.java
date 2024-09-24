158a0a057565d0df55ce6177539e95520a64f19c
Vismit Mandlik <vismitmandlik03.com>
2024-09-24 18:22:03 +0530

    Initial commit

--- src/main/java/org/example/hwclient.java
+++ src/main/java/org/example/hwclient.java
@@ -4,26 +4,26 @@ import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
 import org.zeromq.ZContext;
 
+import java.util.Scanner;
+
 public class hwclient
 {
     public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
         try (ZContext context = new ZContext()) {
             //  Socket to talk to server
-            System.out.println("Connecting to hello world server");
-
             ZMQ.Socket socket = context.createSocket(SocketType.REQ);
             socket.connect("tcp://10.20.40.207:5555");
+            System.out.println("Connection established " );
 
-            for (int requestNbr = 0; requestNbr != 10; requestNbr++) {
-                String request = "Hello";
-                System.out.println("Sending Hello " + requestNbr);
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.print("You: ");
+                String request = sc.nextLine();
                 socket.send(request.getBytes(ZMQ.CHARSET), 0);
-
                 byte[] reply = socket.recv(0);
                 System.out.println(
-                        "Received " + new String(reply, ZMQ.CHARSET) + " " +
-                                requestNbr
+                        "Received: " + new String(reply, ZMQ.CHARSET)
                 );
             }
         }
--- src/main/java/org/example/hwserver.java
+++ src/main/java/org/example/hwserver.java
@@ -4,24 +4,27 @@ import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
 import org.zeromq.ZContext;
 
+import java.util.Scanner;
+
 public class hwserver
 {
     public static void main(String[] args) throws Exception
     {
+        Scanner sc = new Scanner(System.in);
+
         try (ZContext context = new ZContext()) {
             // Socket to talk to clients
             ZMQ.Socket socket = context.createSocket(SocketType.REP);
             socket.bind("tcp://10.20.40.207:5555");
+            System.out.println("Connection established " );
 
             while (!Thread.currentThread().isInterrupted()) {
                 byte[] reply = socket.recv(0);
                 System.out.println(
-                        "Received " + ": [" + new String(reply, ZMQ.CHARSET) + "]"
+                        "Received: " + new String(reply, ZMQ.CHARSET)
                 );
-
-                Thread.sleep(1000); //  Do some 'work'
-
-                String response = "world";
+                System.out.print("You: ");
+                String response = sc.nextLine();
                 socket.send(response.getBytes(ZMQ.CHARSET), 0);
             }
         }
